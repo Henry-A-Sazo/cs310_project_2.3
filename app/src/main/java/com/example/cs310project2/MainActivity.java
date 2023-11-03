7a6539a17ee1c7156ee372b20071a571b48f948d
Henry Sazo <Hsazo@usc.edu>
2023-11-03 00:09:53 -0700

    Sign Up and sign in page done

--- app/src/main/java/com/example/cs310project2/MainActivity.java
+++ app/src/main/java/com/example/cs310project2/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.cs310project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button sign_up_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initializing the button for sign up
+        sign_up_btn = (Button) findViewById(R.id.signup_btn);
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignUp();
+            }
+        });
+
+    }
+
+    public void openSignUp(){
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
