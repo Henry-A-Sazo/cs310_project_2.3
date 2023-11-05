e10409e1eacf4ca72d5e597b54104a28d1ec5e49
Henry Sazo <Hsazo@usc.edu>
2023-11-04 21:05:57 -0700

    crap ton of files

---
+++ app/src/main/java/com/example/cs310project2/home.java
@@ -0,0 +1,67 @@
+package com.example.cs310project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View; // Add this import
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast; // Add this import
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class home extends AppCompatActivity {
+
+    public Button friends_btn;
+    public Button meetings_btn;
+    public Button profile_btn;
+
+    ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        friends_btn = (Button) findViewById(R.id.friends_btn);
+        friends_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFriends();
+            }
+        });
+
+        meetings_btn = (Button) findViewById(R.id.meetings_btn);
+        meetings_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMeetings();
+            }
+        });
+
+        profile_btn = (Button) findViewById(R.id.profile_btn);
+        profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProfile();
+            }
+        });
+    }
+
+    public void openFriends(){
+        Intent intent = new Intent(this, friends.class);
+        startActivity(intent);
+    }
+
+    public void openMeetings(){
+        Intent intent = new Intent(this, mettings.class);
+        startActivity(intent);
+    }
+    public void openProfile(){
+        Intent intent = new Intent(this, profile.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/cs310project2/profile.java
@@ -0,0 +1,15 @@
+package com.example.cs310project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class profile extends AppCompatActivity {
+public Button update_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
