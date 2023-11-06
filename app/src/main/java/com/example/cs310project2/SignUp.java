216d0165bfa6b633dfda0c8012205317f9e54f58
Henry Sazo <Hsazo@usc.edu>
2023-11-06 12:18:17 -0800

    interests added to sign up

--- app/src/main/java/com/example/cs310project2/SignUp.java
+++ app/src/main/java/com/example/cs310project2/SignUp.java
@@ -1,6 +1,8 @@
 package com.example.cs310project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -55,5 +57,8 @@ public class SignUp extends AppCompatActivity {
         String status = statusEditText.getText().toString();
         reference = root.getReference("users/"+email+"/status");
         reference.setValue(status);
+
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
     }
 }
