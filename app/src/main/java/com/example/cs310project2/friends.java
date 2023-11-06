a4964a3d9b8db1070e4fca9a5566e1e79063a4ad
Henry Sazo <Hsazo@usc.edu>
2023-11-06 12:41:34 -0800

    setup for friends is up

--- app/src/main/java/com/example/cs310project2/friends.java
+++ app/src/main/java/com/example/cs310project2/friends.java
@@ -2,13 +2,57 @@ package com.example.cs310project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class friends extends AppCompatActivity {
+    public Button friends_btn;
+    public Button meetings_btn;
+    public Button profile_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        friends_btn = (Button) findViewById(R.id.friends_btn);
+        friends_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFriends();
+            }
+        });
+
+        meetings_btn = (Button) findViewById(R.id.meetings_btn);
+        meetings_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMeetings();
+            }
+        });
+
+        profile_btn = (Button) findViewById(R.id.profile_btn);
+        profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProfile();
+            }
+        });
+    }
+
+    public void openFriends(){
+        Intent intent = new Intent(this, friends.class);
+        startActivity(intent);
+    }
+
+    public void openMeetings(){
+        Intent intent = new Intent(this, meetings.class);
+        startActivity(intent);
+    }
+    public void openProfile(){
+        Intent intent = new Intent(this, profile.class);
+        startActivity(intent);
     }
 }
