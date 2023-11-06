0df374f9195ae30cb5ae45e42757dfc7e8cfc628
Henry Sazo <Hsazo@usc.edu>
2023-11-06 13:28:15 -0800

    meetings tab is set up

--- app/src/main/java/com/example/cs310project2/home.java
+++ app/src/main/java/com/example/cs310project2/home.java
@@ -19,6 +19,12 @@ public class home extends AppCompatActivity {
     public Button meetings_btn;
     public Button profile_btn;
 
+    public Button meeting1;
+    public Button meeting2;
+    public Button meeting3;
+    public Button meeting4;
+
+
     ListView list;
 
     @Override
@@ -49,6 +55,38 @@ public class home extends AppCompatActivity {
                 openProfile();
             }
         });
+
+        meeting1 = (Button) findViewById(R.id.meeting_1);
+        meeting1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                meeting1_open();
+            }
+        });
+
+        meeting2 = (Button) findViewById(R.id.meeting_2);
+        meeting2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                meeting2_open();
+            }
+        });
+
+        meeting3 = (Button) findViewById(R.id.meeting_3);
+        meeting3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                meeting3_open();
+            }
+        });
+
+        meeting4 = (Button) findViewById(R.id.meeting_4);
+        meeting4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                meeting4_open();
+            }
+        });
     }
 
     public void openFriends(){
@@ -62,6 +100,27 @@ public class home extends AppCompatActivity {
     }
     public void openProfile(){
         Intent intent = new Intent(this, profile.class);
+        startActivity(intent);
+    }
+
+    //opening the meetings
+    public void meeting1_open(){
+        Intent intent = new Intent(this, metting1.class);
+        startActivity(intent);
+    }
+
+    public void meeting2_open(){
+        Intent intent = new Intent(this, meeting2.class);
+        startActivity(intent);
+    }
+
+    public void meeting3_open(){
+        Intent intent = new Intent(this, meeting3.class);
+        startActivity(intent);
+    }
+
+    public void meeting4_open(){
+        Intent intent = new Intent(this, meeting4.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cs310project2/profile.java
+++ app/src/main/java/com/example/cs310project2/profile.java
@@ -2,7 +2,9 @@ package com.example.cs310project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class profile extends AppCompatActivity {
@@ -11,5 +13,20 @@ public Button update_btn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        update_btn = (Button) findViewById(R.id.update_btn);
+
+        update_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update_();
+            }
+        });
+    }
+
+
+    public void update_(){
+        Intent intent = new Intent(this, update.class);
+        startActivity(intent);
     }
 }
