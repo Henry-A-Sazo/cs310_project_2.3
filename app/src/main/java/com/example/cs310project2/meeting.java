41fa3266d42210d8693c8b159bb1ea91e636ed14
Carlos Kiran Subramanian Vidal <cksubram@usc.edu>
2023-11-06 01:47:12 -0800

    meetings mostly complete

--- app/src/main/java/com/example/cs310project2/meeting.java
+++ app/src/main/java/com/example/cs310project2/meeting.java
@@ -1,24 +1,17 @@
 package com.example.cs310project2;
 
-import android.content.Intent;
-import android.util.Log;
 import java.util.ArrayList;
 import java.lang.*;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
 
 
 public class meeting {
-
-    private FirebaseDatabase root;
-
     //basic information about the meeting
     public String host;
     private String location;
     private String time;
     private String topic;
     private ArrayList<String> members;
-    private String id;
+    private final String id;
 
     //Constructor
     meeting(/*String creator,*/ String oLocation, String oTime, String oTopic) {
@@ -28,9 +21,7 @@ public class meeting {
         topic = oTopic;
         members = new ArrayList<>();
         members.add(host);
-
-        //save the meeting on firebase and set the id
-        AddtoFireBase();
+        id = String.valueOf(System.currentTimeMillis());
     }
 
     //basic getters and functions for the meeting
@@ -49,9 +40,12 @@ public class meeting {
         return members;
     }
 
+    public String getID() {
+        return id;
+    }
+
     public void joinMeeting(String user) {
         members.add(user);
-        AddtoFireBase();
     }
 
     public void leaveMeeting(String user) {
@@ -61,7 +55,6 @@ public class meeting {
         } else if(user.equals(host)) {
             host = "";
         }
-        AddtoFireBase();
     }
 
     public void updateMeeting(String user, String newTime, String newLocation, String newTopic) {
@@ -75,28 +68,6 @@ public class meeting {
             if(!newTopic.equals("")) {
                 topic = newTopic;
             }
-
-            AddtoFireBase();
         }
-    }
-
-    private void AddtoFireBase() {
-        root = FirebaseDatabase.getInstance();
-
-
-        if(id == null || id.equals("")) { //create a nonce
-            id = String.valueOf(System.currentTimeMillis());
-        }
-        Log.d("create", "Test");
-        DatabaseReference meetingRef = root.getReference("meetings/" + id + "/host");
-        meetingRef.setValue(host);
-        meetingRef = root.getReference("meetings/" + id + "/location");
-        meetingRef.setValue(location);
-        meetingRef = root.getReference("meetings/" + id + "/time");
-        meetingRef.setValue(time);
-        meetingRef = root.getReference("meetings/" + id + "/topic");
-        meetingRef.setValue(topic);
-        meetingRef = root.getReference("meetings/" + id + "/members");
-        meetingRef.setValue(members);
     }
 }
--- app/src/main/java/com/example/cs310project2/meetings.java
+++ app/src/main/java/com/example/cs310project2/meetings.java
@@ -2,21 +2,29 @@ package com.example.cs310project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class meetings extends AppCompatActivity {
 
     private Button create_btn;
+
+    private FirebaseDatabase root;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meetings);
 
-
+        root = FirebaseDatabase.getInstance();
+        reference = null;
         create_btn = (Button) findViewById(R.id.create_btn);
         create_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +42,14 @@ public class meetings extends AppCompatActivity {
         String time = timeEditText.getText().toString();
         EditText topicEditText = findViewById(R.id.topic_input);
         String topic = topicEditText.getText().toString();
-        if(!validateInfo(location, time, topic)) {
-            //meeting could not be created
-        } else {
+        if(validateInfo(location, time, topic)) {
+            Log.d("create", "gjejfejge");
             meeting m = new meeting(location, time, topic);
+            AddToDatabase(m);
             //Intent intent = new Intent(this, home.class);
             //startActivity(intent);
+        } else {//meeting could not be created
+
         }
     }
 
@@ -50,5 +60,21 @@ public class meetings extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private void AddToDatabase(meeting m) {
+        String id = m.getID();
+        Log.d("create", id);
+        reference = root.getReference("meetings/"+id+"/host");
+        reference.setValue(m.host);
+        reference = root.getReference("meetings/"+id+"/location");
+        reference.setValue(m.getLocation());
+        reference = root.getReference("meetings/"+id+"/time");
+        reference.setValue(m.getTime());
+        reference = root.getReference("meetings/"+id+"/topic");
+        reference.setValue(m.getTopic());
+        reference = root.getReference("meetings/"+id+"/members");
+        reference.setValue(m.getMembers());
+
     }
 }
