6ee0b09471de0ab5674708e684206e09c3a07274
Carlos Kiran Subramanian Vidal <cksubram@usc.edu>
2023-11-06 22:17:23 -0800

    carlos

--- app/src/main/java/com/example/cs310project2/MainActivity.java
+++ app/src/main/java/com/example/cs310project2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cs310project2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     public Button sign_up_btn;
     public Button sign_in_btn;
+
+    private User currUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +54,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openHome(){
-        Intent intent = new Intent(this, home.class);
-        startActivity(intent);
+        EditText emailEditText = findViewById(R.id.si_email_input);
+        String email = emailEditText.getText().toString();
+        //reference = root.getReference("users/" + username[0] + "/name");
+        String[] username = email.split("@");
+
+        EditText passwordEditText = findViewById(R.id.si_password_input);
+        String password = passwordEditText.getText().toString();
+        //reference = root.getReference("users/" + username[0] + "/name");
+
+
+        FirebaseDatabase root = FirebaseDatabase.getInstance();
+        DatabaseReference reference = root.getReference("users/" + username);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                currUser = snapshot.getValue(User.class);
+
+                if(currUser != null) {
+                    Log.d("create", "test");
+                    if (password.equals(currUser.getPassword())) {
+
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cs310project2/email.java
+++ app/src/main/java/com/example/cs310project2/email.java
@@ -1,17 +1,60 @@
 package com.example.cs310project2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.EditText;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class email extends AppCompatActivity {
 public Button send_btn;
 
+    User currUser;
+    FirebaseDatabase root;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("user");
+        root = FirebaseDatabase.getInstance();
+        reference = root.getReference("users/" + userName);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                currUser = snapshot.getValue(User.class);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
 
@@ -24,7 +67,66 @@ public Button send_btn;
         });
     }
     public void sendInvite(){
+
+        //first get the string from the textedit
+        EditText emailEditText = findViewById(R.id.email_input);
+        String email = emailEditText.getText().toString();
+
+        final String username = "310talk2friends@gmail.com";
+        final String password = "CarlosChristianHenry";
+
+        final String messageBody = "Hello,\n Your Friend " + currUser.getName() + " has invited you to Talk2Friends!\n" + "Your username is the email on which you have recieved this, and your current password is " + ".\n" + "Please login and change your password";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+
+
+        new AsyncTask<Void, Void, Exception>() {
+            @Override
+            protected Exception doInBackground(Void... voids) {
+                try {
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(username));
+                    message.setRecipients(Message.RecipientType.TO,
+                            InternetAddress.parse(email));
+                    message.setSubject("Invitation to Talk2Friends");
+                    message.setText(messageBody);
+                    Transport.send(message);
+                    return null; // No exception, email sent successfully
+                } catch (MessagingException e) {
+                    return e; // Return the caught exception to be processed in onPostExecute
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Exception e) {
+                // onPostExecute runs on the main thread, so we can update the UI
+                if (e == null) {
+                    Log.d("create", "email success");
+                    // Start the next activity only after email has been sent successfully
+                    Intent intent = new Intent(email.this, friends.class);
+                    intent.putExtra("user", currUser.getEmail());
+                    startActivity(intent);
+                } else {
+                    // Log the exception and notify the user
+                    Log.d("create", "email error", e);
+                }
+            }
+        }.execute();
+
         Intent intent = new Intent(this, friends.class);
+        intent.putExtra("user", currUser.getEmail());
         startActivity(intent);
     }
 }
