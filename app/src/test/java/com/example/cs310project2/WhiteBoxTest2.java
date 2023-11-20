3d79c8c9a7c8328f0e3ac953b11f7ebdfc1b4a43
Henry Sazo <Hsazo@usc.edu>
2023-11-19 16:07:12 -0800

    white box test for sign in

--- app/src/test/java/com/example/cs310project2/WhiteBoxTest2.java
+++ app/src/test/java/com/example/cs310project2/WhiteBoxTest2.java
@@ -1,13 +1,28 @@
 package com.example.cs310project2;
+
 import android.widget.EditText;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class WhiteBoxTest2 {
+    private User currUser;
+
     @Test
     public void testEmailEditText() {
-        // Replace R.id.emailEditText with the actual ID of your EditText
+        // Replace R.id.email_input with the actual ID of your EditText
         EditText emailEditText = new EditText(/* context */ null);
-        emailEditText.setId(R.id.email_input);
+        emailEditText.setId(com.example.cs310project2.R.id.email_input);
 
         // Test a valid email
         String validEmail = "test@usc.edu";
@@ -30,5 +45,78 @@ public class WhiteBoxTest2 {
         // This is a simple example and may not cover all edge cases
         String emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}";
         return email.matches(emailPattern);
+    }
+
+    @Test
+    public void testButtonClicks() {
+        YourActivity activity = new YourActivity();  // Replace with the actual activity class
+        MockButton button1 = new MockButton();
+        MockButton button2 = new MockButton();
+        MockButton button3 = new MockButton();
+
+        // Replace R.id.meetings_btn, R.id.profile_btn, and R.id.friends_btn
+        // with the actual IDs or references of your buttons
+        activity.setMeetingButton(button1);
+        activity.setProfileButton(button2);
+        activity.setFriendsButton(button3);
+
+        // Simulate button clicks
+        activity.onMeetingButtonClick();
+        activity.onProfileButtonClick();
+        activity.onFriendsButtonClick();
+
+        // Check if the corresponding methods were called
+        assertTrue(button1.isClicked());
+        assertTrue(button2.isClicked());
+        assertTrue(button3.isClicked());
+    }
+
+    // Example MockButton class to simulate button behavior
+    private static class MockButton {
+        private boolean clicked = false;
+
+        public void click() {
+            clicked = true;
+        }
+
+        public boolean isClicked() {
+            return clicked;
+        }
+    }
+
+    // Replace this with the actual class name
+    private static class YourActivity {
+        private MockButton meetingButton;
+        private MockButton profileButton;
+        private MockButton friendsButton;
+
+        // Setter methods for buttons
+        public void setMeetingButton(MockButton meetingButton) {
+            this.meetingButton = meetingButton;
+        }
+
+        public void setProfileButton(MockButton profileButton) {
+            this.profileButton = profileButton;
+        }
+
+        public void setFriendsButton(MockButton friendsButton) {
+            this.friendsButton = friendsButton;
+        }
+
+        // Methods triggered by button clicks
+        public void onMeetingButtonClick() {
+            // Perform actions associated with meeting button click
+            meetingButton.click();
+        }
+
+        public void onProfileButtonClick() {
+            // Perform actions associated with profile button click
+            profileButton.click();
+        }
+
+        public void onFriendsButtonClick() {
+            // Perform actions associated with friends button click
+            friendsButton.click();
+        }
     }
 }
