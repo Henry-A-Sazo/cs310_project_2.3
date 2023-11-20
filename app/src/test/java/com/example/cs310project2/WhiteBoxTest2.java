d9599d1be404f320c23a547f96057addf1cfa5fa
Henry Sazo <Hsazo@usc.edu>
2023-11-19 16:07:51 -0800

    white box test for sign in

--- app/src/test/java/com/example/cs310project2/WhiteBoxTest2.java
+++ app/src/test/java/com/example/cs310project2/WhiteBoxTest2.java
@@ -2,12 +2,6 @@ package com.example.cs310project2;
 
 import android.widget.EditText;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
