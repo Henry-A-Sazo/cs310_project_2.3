3dfd0002c1df0580dfe91af7474a9ec33b9275ee
Henry Sazo <Hsazo@usc.edu>
2023-11-19 22:31:44 -0800

    white box test for sign in

--- app/src/test/java/com/example/cs310project2/WhiteBoxTest2.java
+++ app/src/test/java/com/example/cs310project2/WhiteBoxTest2.java
@@ -2,9 +2,7 @@ package com.example.cs310project2;
 
 import android.widget.EditText;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -111,6 +109,160 @@ public class WhiteBoxTest2 {
         public void onFriendsButtonClick() {
             // Perform actions associated with friends button click
             friendsButton.click();
+        }
+    }
+
+    @Test
+    public void testUpdateButtonPress() {
+        // Create an instance of your class containing the button press logic
+        YourClassUnderTest classUnderTest = new YourClassUnderTest();
+
+        // Call the method associated with the button press for the update button
+        classUnderTest.onUpdateButtonClick();
+
+        // Assert that the internal state or behavior has been modified as expected
+        assertTrue(classUnderTest.isUpdateButtonPressed());
+    }
+
+    // Replace this with the actual class name containing your button press logic
+    private static class YourClassUnderTest {
+        private boolean updateButtonPressed = false;
+
+        // Method associated with the update button press
+        public void onUpdateButtonClick() {
+            // Perform actions associated with the update button press
+            updateButtonPressed = true;
+        }
+
+        // Getter method to check the internal state
+        public boolean isUpdateButtonPressed() {
+            return updateButtonPressed;
+        }
+    }
+
+    @Test
+    public void testEnteredTextInEditTexts() {
+        // Create an instance of your class containing the EditText widgets
+        yourClassUnderTest classUnderTest = new yourClassUnderTest();
+
+        // Set text in the first EditText
+        classUnderTest.setEditText1Text("passwordtest");
+
+        // Set text in the second EditText
+        classUnderTest.setEditText2Text("23");
+
+        // Set text in the third EditText
+        classUnderTest.setEditText3Text("Native");
+
+        // Retrieve and check the text from the first EditText
+        assertEquals("passwordtest", classUnderTest.getEditText1Text());
+
+        // Retrieve and check the text from the second EditText
+        assertEquals("23", classUnderTest.getEditText2Text());
+
+        // Retrieve and check the text from the third EditText
+        assertEquals("Native", classUnderTest.getEditText3Text());
+    }
+
+    // Replace this with the actual class name containing your EditText widgets
+    private static class yourClassUnderTest {
+        private EditText editText1;
+        private EditText editText2;
+        private EditText editText3;
+
+        // Setter methods for EditTexts
+        public void setEditText1(EditText editText) {
+            this.editText1 = editText;
+        }
+
+        public void setEditText2(EditText editText) {
+            this.editText2 = editText;
+        }
+
+        public void setEditText3(EditText editText) {
+            this.editText3 = editText;
+        }
+
+        // Setter methods for text in EditTexts
+        public void setEditText1Text(String text) {
+            editText1.setText(text);
+        }
+
+        public void setEditText2Text(String text) {
+            editText2.setText(text);
+        }
+
+        public void setEditText3Text(String text) {
+            editText3.setText(text);
+        }
+
+        // Getter methods for text in EditTexts
+        public String getEditText1Text() {
+            return editText1.getText().toString();
+        }
+
+        public String getEditText2Text() {
+            return editText2.getText().toString();
+        }
+
+        public String getEditText3Text() {
+            return editText3.getText().toString();
+        }
+    }
+
+    @Test
+    public void testCheckBoxBehavior() {
+        // Create an instance of your class containing the CheckBox widgets
+        YourclassUnderTest classUnderTest = new YourclassUnderTest();
+
+        // Check the first checkbox
+        classUnderTest.checkCheckBox1();
+
+        // Check the second checkbox
+        classUnderTest.checkCheckBox2();
+
+        // Uncheck the third checkbox
+        classUnderTest.uncheckCheckBox3();
+
+        // Assert that the state of checkboxes is as expected
+        assertTrue(classUnderTest.isCheckBox1Checked());
+        assertTrue(classUnderTest.isCheckBox2Checked());
+        assertFalse(classUnderTest.isCheckBox3Checked());
+    }
+
+    // Replace this with the actual class name containing your CheckBox widgets
+    private static class YourclassUnderTest {
+        private boolean checkBox1Checked = false;
+        private boolean checkBox2Checked = false;
+        private boolean checkBox3Checked = false;
+
+        // Methods to interact with CheckBoxes
+        public void checkCheckBox1() {
+            // Perform actions associated with checking the first CheckBox
+            checkBox1Checked = true;
+        }
+
+        public void checkCheckBox2() {
+            // Perform actions associated with checking the second CheckBox
+            checkBox2Checked = true;
+        }
+
+        public void uncheckCheckBox3() {
+            // Perform actions associated with unchecking the third CheckBox
+            checkBox3Checked = false;
+        }
+
+        // Getter methods to check the state of CheckBoxes
+        public boolean isCheckBox1Checked() {
+            return checkBox1Checked;
+        }
+
+        public boolean isCheckBox2Checked() {
+            return checkBox2Checked;
+        }
+
+        public boolean isCheckBox3Checked() {
+            return checkBox3Checked;
         }
     }
 }
